f316b41eacafc527ba4a030598cce4787bb43fc7
JOSE ELIAS SILVA SOUSA <zeliassilva005@gmail.com>
2024-11-11 21:03:49 -0300

    alteracao

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get( '/', async(req, res) => {
+return res.status(200).json({message : "api rodando com sucesso"})
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
